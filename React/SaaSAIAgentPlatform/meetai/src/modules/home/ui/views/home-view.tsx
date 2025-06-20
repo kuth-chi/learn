cec5f6e0f543445b4d3ba608b06d201513817143
Kuth <69514175+kuth-chi@users.noreply.github.com>
2025-06-20 21:29:07 +0700

    modified:   package-lock.json 	modified:   package.json 	new file:   src/app/api/trpc/[trpc]/route.ts 	modified:   src/app/layout.tsx 	modified:   src/modules/home/ui/views/home-view.tsx 	new file:   src/trpc/client.tsx 	new file:   src/trpc/init.ts 	new file:   src/trpc/query-client.ts 	new file:   src/trpc/routers/_app.ts 	new file:   src/trpc/server.tsx

--- React/SaaSAIAgentPlatform/meetai/src/modules/home/ui/views/home-view.tsx
+++ React/SaaSAIAgentPlatform/meetai/src/modules/home/ui/views/home-view.tsx
@@ -1,31 +1,14 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomePageView = () => {
-    const router = useRouter();
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({text: "Kuth Chi"}));
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Welcome to MeetAI</h1>
-      <p className="text-lg text-gray-600 mb-8">Your AI-powered meeting assistant</p>
-      <h2></h2>
-      <Button
-        onClick={() => {
-          // Handle sign out logic here
-          authClient.signOut({
-            fetchOptions: {
-                onSuccess: () => {
-                    router.push("/sign-in");
-                }
-            }
-          })
-          }}
-        variant="default" className="cursor-pointer"
-      >
-       Sing out
-      </Button>
+    <div className="flex flex-col p-4 gap-y-4">
+      {data?.greeting}
     </div>
   );
 }
